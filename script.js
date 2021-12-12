f0c115cc4019ea6efa4cd6d5c1c7ffa77add4660
joaquin <jecorrea@protonmail.com>
2021-12-12 11:01:49 -0500

    Added functionality to make messages dissapear

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ const button = document.getElementById("button");
 const toasts = document.getElementById("toasts");
 
 const messages = [
-  "Message One",
-  "Message Two",
-  "Message Three",
-  "Message Four",
+  "Burn, feel no shame or pity. Burn, apocalyptic city",
+  "Expect no thanks, you can kiss my ass.",
+  "Fallen saint, Fallen saint, Fallen saint ' Fallen!",
+  "Go and give 'em war! True bloody war!",
 ];
 
 button.addEventListener("click", () => createNotification());
@@ -14,7 +14,13 @@ function createNotification() {
   const notif = document.createElement("div");
   notif.classList.add("toast");
 
-  notif.innerText = getRandomMessage();
+  notif.innerText = getRandomMessage(); //Create the message from the messages arr
+  toasts.appendChild(notif); //Append it to the DOM
+
+  //To remove messages from the DOM
+  setTimeout(() => {
+    notif.remove();
+  }, 3000);
 }
 
 function getRandomMessage() {
